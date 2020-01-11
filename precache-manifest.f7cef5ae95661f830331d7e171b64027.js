f8416b34deb1b2a825448d96847157f64b2a53c7
unknown <harsha19990226@gmail.com>
2020-01-11 11:31:09 +0530

    Initial dist subtree commit

---
+++ precache-manifest.f7cef5ae95661f830331d7e171b64027.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc820a193b3aec6ea7fc30cf53c53b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3242862ba30d0981c48",
+    "url": "/static/css/2.15e304cb.chunk.css"
+  },
+  {
+    "revision": "28a8fe434fa5601c295e",
+    "url": "/static/css/main.31685162.chunk.css"
+  },
+  {
+    "revision": "c3242862ba30d0981c48",
+    "url": "/static/js/2.1a4f61fd.chunk.js"
+  },
+  {
+    "revision": "28a8fe434fa5601c295e",
+    "url": "/static/js/main.4ca61dd8.chunk.js"
+  },
+  {
+    "revision": "287609bed90ed5d1ec6d",
+    "url": "/static/js/runtime~main.0a9263ab.js"
+  },
+  {
+    "revision": "b1ed222892fcc5cf15c2bb5cea321a93",
+    "url": "/static/media/event_1.b1ed2228.jpg"
+  },
+  {
+    "revision": "2f74b19ba2241cb3565aa21a431172e6",
+    "url": "/static/media/event_2.2f74b19b.jpg"
+  },
+  {
+    "revision": "4462d574118a18018d87d9e71e040ad0",
+    "url": "/static/media/event_3.4462d574.jpg"
+  },
+  {
+    "revision": "fbaee159f93f5de16835766d15c3886b",
+    "url": "/static/media/events.fbaee159.png"
+  },
+  {
+    "revision": "a880eeeefb11a656693b57f8b579185f",
+    "url": "/static/media/loader.a880eeee.gif"
+  },
+  {
+    "revision": "1945e65de3962fa564ca2e7e8fd85c78",
+    "url": "/static/media/scroll-bg.1945e65d.png"
+  }
+]);
